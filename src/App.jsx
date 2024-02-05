9396f9d7c9f808caef143c400553d0bb9ecc086f
Katherine <katlim89@gmail.com>
2024-02-05 18:09:51 +0800

    Added some styling to Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ function Experience() {
   return (
     <>
       <h3>Experience</h3>
-      <div className="exp-header">Inari Technology Sdn Bhd</div>
-      <p>Process Engineer</p>
+      <h4 style={{ backgroundColor: "#d9d9d9" }}>Inari Technology Sdn Bhd</h4>
+      <p><strong><em>Process Engineer</em></strong></p>
       <ul>
         <li>Buy-off Automated Optical Inspection (AOI) machine for production use; including preparation of one-point lessons, work instructions and log records</li>
         <li>Set-up weighing station for production use; including documentation of weight specifications for all products</li>
@@ -11,16 +11,16 @@ function Experience() {
         <li>Prepared and distributed weekly reports for production cycle time and Automated Visual Inspection (AVI) yield</li>
         <li>Coordinated inter-departmental Manufacturing Execution System (MES) projects</li>
       </ul>
-      <div className="exp-header">Koon Hoe Enterprise Sdn Bhd</div>
-      <p>General Clerk</p>
+      <h4 style={{ backgroundColor: "#d9d9d9" }}>Koon Hoe Enterprise Sdn Bhd</h4>
+      <p><strong><em>General Clerk</em></strong></p>
       <ul>
         <li>Prepared price quotations for customers</li>
         <li>Managed customers orders; including payment collection</li>
         <li>Recorded monthly sales and general accounts balancing</li>
         <li>Performed regular in-house stock checking</li>
       </ul>
-      <div className="exp-header">Knowles Electronics (M) Sdn Bhd</div>
-      <p>Intern</p>
+      <h4 style={{ backgroundColor: "#d9d9d9" }}>Knowles Electronics (M) Sdn Bhd</h4>
+      <p><strong><em>Intern</em></strong></p>
       <ul>
         <li>Learnt Quality Assurance (QA) process work flow from receiving incoming raw material to manufacturing end product</li>
         <li>Gained hands-on knowledge in geometric dimension measurement of mechanical parts</li>
